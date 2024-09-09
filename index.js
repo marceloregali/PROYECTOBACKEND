7302bcfc0017d114d612dad26a33ef78d09c185a
Marcelo Regali <marcelo8924@hotmail.com>
2024-09-09 20:42:06 -0300

    Configuración de Handlebars y ajustes de seguridad

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ import helmet from "helmet"; // políticas de seguridad
 import productsRouter from "./routes/products.js";
 import cartsRouter from "./routes/carts.js";
 import usersRouter from "./routes/users.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import expressHandlebars from "express-handlebars";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
+
+// Configuración de Handlebars
+const hbs = expressHandlebars.create({
+  layoutsDir: __dirname + "/layouts",
+  defaultLayout: "main",
+  extname: ".handlebars",
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/layouts");
 
 // Middleware para manejar JSON
 app.use(express.json());
