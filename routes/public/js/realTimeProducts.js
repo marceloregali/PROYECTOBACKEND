298801e3f126932252fe18baea1fafd97d76da45
Marcelo Regali <marcelo8924@hotmail.com>
2024-09-12 22:08:58 -0300

    avanceFinal

--- routes/public/js/realTimeProducts.js
+++ routes/public/js/realTimeProducts.js
@@ -1,14 +1,12 @@
-const socket = io(); // Conéctate al servidor de WebSocket
+const socket = io();
 
-// Escucha el evento 'updateProducts' emitido por el servidor
 socket.on("updateProducts", (products) => {
   const productsList = document.getElementById("products-list");
-  productsList.innerHTML = ""; // Limpia la lista actual
+  productsList.innerHTML = "";
 
-  // Itera sobre los productos y agrega cada uno a la lista
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.textContent = `${product.title} - $${product.price}`; // Asegúrate de que las propiedades coincidan con las del objeto producto
+    li.textContent = `${product.title} - $${product.price}`;
     productsList.appendChild(li);
   });
 });
--- routes/views.js
+++ routes/views.js
@@ -3,10 +3,9 @@ const router = Router();
 
 router.get("/home", async (req, res) => {
   try {
-    // Obtener la lista de productos desde la API
     const response = await fetch("http://localhost:8080/api/products");
     const productos = await response.json();
-    // Renderizar la vista 'home.handlebars' con los productos
+
     res.render("home", { productos });
   } catch (error) {
     console.error("Error al obtener productos:", error);
